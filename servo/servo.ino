924ef21e6abf827b32eacf179278f0317e12196f
Adele <AdelBeit@users.noreply.github.com>
2019-04-05 21:57:39 -0400

    Update servo.ino

--- servo/servo.ino
+++ servo/servo.ino
@@ -11,13 +11,18 @@ void setup()
   // the 1000 & 2000 set the pulse width 
   // mix & max limits, in microseconds.
   // Be careful with shorter or longer pulses.
-  servo1.attach(servo_PIN, 1000, 2000);
+  servo1.attach(servo_PIN);//, 1000, 2000);
   Serial.begin(9600);
   next = millis() + 500;
 }
 
 void loop()
 {
+  int i = 500;
+  servo1.write(180);
+  delay(i);
+  servo1.write(0);
+  delay(i);
 //  static bool rising = true;
 //
 //  if(millis() > next)
@@ -41,9 +46,4 @@ void loop()
 //    servo1.write(i);
 //    delay(1000);
 //  }
-  int i = 500;
-  servo1.write(180);
-  delay(i);
-  servo1.write(0);
-  delay(i); 
 }
